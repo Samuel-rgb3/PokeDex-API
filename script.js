67315e308a6dc7b4cd1d2f576f06788b79eb2b27
SAMUEL RENILDO DE ALMEIDA NOGUEIRA <a95984530@rn.senac.local>
2026-04-08 21:13:03 -0300

    Programação incompleta do script.js, por causa do horário

---
+++ script.js
@@ -0,0 +1,19 @@
+const formulario = document.querySelector(".formulario");
+const areaResultado = document.querySelector("#area-resultado");
+const areaErro = document.querySelector("#area-erro");
+const inputPokemon = document.querySelector("#input-pokemon");
+const btnBuscar = document.querySelector("#btn-buscar");
+
+
+btnBuscar.addEventListener("click", function(){
+    const nomePokemon = inputPokemon.value
+
+    if (nomePokemon.trim() === "") {
+        return
+    }
+});
+    buscarPokemon.(nomePokemon) {
+        async function buscarPokemon(nomePokemon) {
+            
+        }
+    }
